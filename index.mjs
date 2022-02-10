463785c7e11630427efa05b1ed36eee2350f298f
cb <digplan@outlook.com>
2022-02-09 19:43:24 -0600

    u

--- index.mjs
+++ index.mjs
@@ -1,6 +1,8 @@
 #!/usr/bin/env node
 console.log('executed with > npx digplan/npx-test')
 
+console.log('working directory is ' + process.cwd())
+
 import C from './class.mjs'
 const i = new C()
 i.func()
