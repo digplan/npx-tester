e7eacdf111e7535ab2c7568bc36bee75ce48e4ea
cb <digplan@outlook.com>
2022-03-01 09:54:52 -0600

    u

--- index.mjs
+++ index.mjs
@@ -1,8 +1,20 @@
 #!/usr/bin/env node
-console.log('executed with > npx digplan/npx-test')
+import path, {resolve} from 'node:path'
+import fs from 'node:fs'
+import {pathToFileURL} from 'node:url'
 
+console.log('executed with > npx digplan/npx-test')
+console.log('working directory is ' + process.cwd())
+console.log('path resolve for class.mjs is ' + resolve('class.mjs'))
+console.log('file url is ' + pathToFileURL(resolve('class.mjs')))
 console.log('working directory is ' + process.cwd())
 
 import C from './class.mjs'
 const i = new C()
 i.func()
+
+console.log('open file using resolve')
+fs.readFileSync(resolve('class.mjs'))
+
+console.log('import file using resolve')
+await import(pathToFileURL(resolve('class.mjs')))
